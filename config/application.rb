f6ac5946f26a2a5f35a65cae5ac62e85e91d033d
k-s-p <haruichiban0920@gmail.com>
2023-04-26 22:51:52 +0900

    slimの導入、bootstrapの導入、エラーメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Studychinese
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # デフォルトのロケールを:en以外に変更する
+    config.i18n.default_locale = :ja
+
+    # I18nライブラリに訳文の探索場所を指示する
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
   end
 end
